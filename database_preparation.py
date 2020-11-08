9288f6d64d064f0d2442172d207a8e7fdec0a445
TDLemonNovecento <leila.stujic@gmail.com>
2020-11-08 20:18:40 +0100

    added file reading and storing functions

--- database_preparation.py
+++ database_preparation.py
@@ -1,5 +1,28 @@
 '''in this file some more or less efficient methods will be written to retrieve xyz data from files, store it in one file to make programs more efficient, and to open such a file with a lot of xyz data and reread it.
 '''
+import pickle
+
+class comound():
+    '''
+    stores information of molecules
+    '''
+    def __init__(self, filename, Z, R, N):
+        self.Z = Z
+        self.R = R
+        self.N = N
+        self.filename = filename
+        self.energy = None
+
+    def add_energy(self, energy):
+        self.energy = energy
+    
+    def heavy_atoms(self):
+        unique, counts = jnp.unique(Z, return_counts = True)
+        dictionary =dict(zip(unique,counts))
+        heavy_atoms = Z.size - dictionary[1]
+        return(heavy_atoms)
+
+    
 
 def read_xyz(folder, representation = jrep.CM_eigenvectors_EVsorted, get_energy = False):
     ''' Opens files in xyz folder and stores Z, R and N data + makes representation
@@ -9,15 +32,13 @@ def read_xyz(folder, representation = jrep.CM_eigenvectors_EVsorted, get_energy 
     representation: some kind of representation that can work with Z, R, N data
     Returns
     -------
-    represented_list: List of representations of the files in file_list
     compound_list: List of information on files in file_list
                     [name of file, Z, R, N]
                     with Z, R and N being numpy arrays and the
                     name of file being a string
     '''
 
-    compound_list = []
-    represented_list = []
+    molecule_list = []
     
     for xyzfile in os.listdir(folder):
         if xyzfile.endswith(".xyz"):
@@ -61,11 +82,43 @@ def read_xyz(folder, representation = jrep.CM_eigenvectors_EVsorted, get_energy 
             R = np.array(R)
             
             #create list of compound information and represented information
-            compound_list.append([xyzfile,Z, R, N, atoms, zero_point_energy])
-            represented = representation(Z,R,N)
-            represented_list.append(represented)
+            molecule_list.append([xyzfile,Z, R, N, atoms, zero_point_energy])
 
-    return(represented_list, compound_list)
+    return(molecule_list)
+
+
+
+def make_compound_instances(molecule_list):
+    '''converts molecular information to compound instances'''
+    dim = len(molecule_list)
+    compound_ls = []
+    heavy_atoms_ls = []
+    for i in range(dim):
+        filename = compound_list[i,0]
+        Z = compound_list[i, 1]
+        R = compound_list[i, 2]
+        N = compound_list[i, 3]
+        atoms = compound_list[i, 4]
+        energy = compound_list[i, 5]
+        c = compound(filename, Z, R, N)
+        c.add_energy(energy)
+        ha = c.heavy_atoms
+        
+        heavy_atoms_ls.append(ha)
+        compound_ls.append(c)
+
+def store_compounds(compound_list, destination_file):
+    '''stores componds to destination_file'''
+    with open(destination_file, 'wb') as f:
+        pickle.dump(compound_list, f)
+
+
+def read_compounds(source_file):
+    '''reads source file and returns compound_list'''
+    with open(source_file, 'rb') as f:
+        compound_list = pickle.load(f)
+
+    return(compound_list)
 
 
 def read_all_xyz(pathway):
